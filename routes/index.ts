54e7a0a2b2315fe3cd4039c13f4199ab83314170
Bishwash <bishwash312@gmail.com>
2024-11-17 22:22:37 +0545

    added rbac in role add

--- routes/index.ts
+++ routes/index.ts
@@ -2,9 +2,10 @@ import router from "../config/router.config";
 import userRoute from "./user.routes";
 import feedbackRoute from "./feedback.routes";
 import roleRoute from "./role.routes";
+import { verifyJWT } from "../middlewares/auth.middleware";
 
 router.use("/api/user", userRoute);
 router.use("/api/feedback", feedbackRoute);
-router.use("/api/role", roleRoute);
+router.use("/api/role", verifyJWT(['superadmin']), roleRoute);
 
 export default router;
