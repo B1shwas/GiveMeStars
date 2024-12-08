b9e4ff88df98ab5ed5669c1858f5d30cffc5ffb9
Bishwash <bishwash312@gmail.com>
2024-12-08 20:31:12 +0545

    change http request

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -9,6 +9,6 @@ import {
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.get("/refresh", refreshTheToken);
+router.post("/refresh", refreshTheToken);
 
 export default router;
